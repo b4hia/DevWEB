ad9109ada401c734e32ded689a516e8fb4c60c88
Gabriel Reis <gabrieldreisilva1@gmail.com>
2023-05-03 22:38:35 -0300

    Add Desafio 3
    Desafio 3 implementação do DB

---
+++ SiteDesafio3/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_Host'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'gabriel'
+app.config['MYSQL_DB'] = 'fatec'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/contato", methods=['POST', 'GET'])
+def contato():
+    if request.method == 'POST':
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['desc']
+
+        cur = mysql.connection.cursor()
+        cur.execute(f'INSERT INTO contatos(email, assunto, descricao) VALUES ("{email}", "{assunto}", "{descricao}")')
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        return 'Concluído'
+    else:
+        return render_template("contato.html")
+
+@app.route("/quemsomos")
+def about():
+    return render_template('quemsomos.html')
+
+@app.route("/users")
+def users():
+    cur = mysql.connection.cursor()
+    users = cur.execute('SELECT * FROM contatos')
+    if users > 0:
+        usersInfo = cur.fetchall()
+        return render_template("users.html", usersInfo=usersInfo)
